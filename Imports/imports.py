2a4d02b9741c06cf7bae775c34a67eae96b9daf8
maxstain <firaschabchoub@hotmail.com>
2024-09-03 10:55:09 +0100

    Added the face recognition.

--- Imports/imports.py
+++ Imports/imports.py
@@ -1,10 +1,11 @@
-import os
-import shutil
-
 try:
     import cv2 as cv
     import os
+    import shutil
     import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    import openpyxl
     from keras import Sequential
     from keras.src.applications.mobilenet_v2 import MobileNetV2
     from keras.src.layers import GlobalAveragePooling2D
@@ -13,6 +14,9 @@ try:
     from keras.src.optimizers import Adam
     from keras.src.layers import Dense
     import pandas as pd
+    import scipy as sp
+    import sklearn as sk
+    import seaborn as sns
 except ImportError:
     print("Error: Missing Required Libraries")
     print("Installing Required Libraries from Requirements.txt")
